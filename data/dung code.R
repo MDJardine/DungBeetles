93d15e1bc3f297fb92d7eb4ccd241e83626ce071
Michael Jardine <michaeljardine92@gmail.com>
2024-06-28 23:05:13 +0100

    deletign unessary olost and redoing the analysis

--- data/dung code.R
+++ data/dung code.R
@@ -1,13 +1,13 @@
 ## packages
 library(ggplot2)
-library(nlme)
 library(lme4)
 library(multcomp)
-library(plotrix)
-library(lsmeans)
-library(aod)
+#library(plotrix)
+#library(lsmeans)
+#library(aod)
 library(Rmisc)
-
+library(RColorBrewer)
+library(pbkrtest)
 
 #### read in the data ####
 setwd("C:/Users/user/Documents/GitHub/DungBeetles/data")
@@ -45,108 +45,104 @@ boxplot(vlost ~ treatment, notch=T, data=dung)
 
 ## does this vary between the rounds?
 boxplot(vlost ~ Round, data=dung)
-## yes but not toomuch or in any clear way
+## yes but not too much or in any clear way
 
-# ggplot
-bp <- ggplot(dung, aes(x = treatment, y = vlost)) +
-  theme_bw() + geom_boxplot() +
-  scale_y_continuous(name="Volume of Dung Removed (L)") +
-  theme(axis.title.y = element_text(face="bold", size=18), 
-        axis.title.x = element_text(face= "bold", size=18), 
-        axis.text.x=element_text(size=15), 
-        axis.text.y=element_text(size=15)) + 
-theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), 
-      panel.background=element_blank(), axis.line=element_line(colour="black")) + 
-  scale_x_discrete(name="Treatment", labels=c("Control", "Fence", "Plate"))
-bp
+## histograms
+hist(dung$vlost)
+# its not overly normal, but we already know it has a bimodal distribution with the treatments
+# kind of looks like two peaks?
 
-## simple linear model as a test
-# full model
-testm1 <- lm(vlost ~ treatment + transcc + plot.lane + Round, data=dung)
-summary(testm1)
-anova (testm1, test="F")
 
-#remove cc
-testm2 <- lm(vlost ~ treatment + plot.lane + Round, data=dung)
-summary(testm2)
-anova (testm2, test="F")
+## so we want to know if the treatment has an effect ion the volume of dung that is lost? 
 
-# remove plot.lane
-testm3 <- lm(vlost ~ treatment + Round, data=dung)
-summary(testm3)
-anova (testm3, test="F")
+## we used multiple rounds of the experiment
+## and we had multiple sites
+## both of these are random variables to control for
+## can fix the random effects structure later
 
-AIC(testm1, testm2, testm3)
-anova(testm1, testm2, testm3)
-##so it seems that the simplest model (testm1) is the best
-## only treatment and round seem relavent
 
 ### mixed models for all 3 treatments
-## want to do with nlme but can't work it out
 # try now with lmer and try to fix later -> same results anyway just p value problem
-model1 <- lmer(vlost ~ treatment*transcc + (1 | Round) + (1 | plot.lane), 
-               data=dung)
-summary(model1)
-anova(model1)
-model1
-
-wald.test(b=fixef(model1), Sigma=vcov(model1), Terms = 3, df=70)
-wald.test(b=fixef(model1), Sigma=vcov(model1), Terms = 4, df=70)
-
-
-## interaction reallu not significant so remove
-model2 <- lmer(vlost ~ treatment + transcc + (1 | Round) + (1 | plot.lane), 
-               data=dung)
-summary(model2)
-anova(model2)
-
-## canopy cover p-value
-wald.test(b=fixef(model2), Sigma=vcov(model2), Terms = 2, df=72)
-
-## doen't seen to work :(
-
-## no effect of canopy cover so simplify again
-model3 <- lmer(vlost ~ treatment + (1 | Round) + (1 | plot.lane), 
-               data=dung)
-summary(model3)
-anova(model3)
-## seems justified by the increasing F value but lets check with anova and AIC
-anova(model1, model2, model3)
-AIC(model1, model2, model3)
-## better AIc, log-likelihood and justified Fvalue for model 3 = the best!!!
-
-## confidence intervals
-confint(model3, method='Wald')
-
-##testing differences between the three treatments
-# Tukey test
-treatmentMCP <- mcp(treatment = 'Tukey')
-tuk <- glht(model3, treatmentMCP)
-confint(tuk)
-tuk
-par(mar=c(5.1, 6.2, 4.1, 2.1))
-plot(tuk)
-summary(tuk)
-tuk
-?glht
-
-## finally lets chekc the model to ake sure its not rubbish
-res<- resid(model3)
-ran <- ranef(model3)
-fit <- fitted(model3)
+model1 <- lmer(vlost ~ treatment*transcc + (1 | Round) + (1 | plot.lane), data=dung)
+## plots
+res<- resid(model1)
+ran <- ranef(model1)
+fit <- fitted(model1)
 par(mfrow=c(1,4))
 hist(ran$Round[,1])
 hist(ran$plot.lane[,1])
 plot(res ~ fit)
 qqnorm(res, main='')
 qqline(res)
+# result
+summary(model1)
+anova(model1)
+model1
+## so it looks like there is no interaction or anything to do with canopy cover 
+# Random effects: round = plot.lane = 
 
+
+#we'll get proper hypothesis testing from bootstrapping since we have random effects
+### check with bootsrapping
+treat3_1.1 <- lmer(vlost ~ 1 + (1 | Round) + (1 | plot.lane), data=dung)
+treat3_2.1 <- lmer(vlost ~ treatment + (1 | Round) + (1 | plot.lane), data=dung)
+treat3_3.1 <- lmer(vlost ~ transcc + (1 | Round) + (1 | plot.lane), data=dung)
+treat3_4.1 <- lmer(vlost ~ treatment+transcc + (1 | Round) + (1 | plot.lane), data=dung)
+treat3_5.1 <- lmer(vlost ~ treatment*transcc + (1 | Round) + (1 | plot.lane), data=dung)
+#treatment
+treat_test <- PBmodcomp(treat3_2.1, treat3_1.1)
+treat_test
+# p< 0.0001
+
+# canopy cover
+cc_test_3 <- PBmodcomp(treat3_3.1, treat3_1.1)
+cc_test_3
+# p = 0.05976
+
+# interaction
+intertest_3 <- PBmodcomp(treat3_5.1, treat3_4.1)
+intertest_3
+# p = 0.9944
+
+# now we can plot this
+ggplot(dung, aes(x = treatment, y = vlost, fill=treatment)) +
+  theme_bw() + geom_boxplot() +
+  scale_y_continuous(name="Volume of Dung Removed (L)") +
+  theme(axis.title.y = element_text(face="bold", size=18), 
+        axis.title.x = element_text(face= "bold", size=18), 
+        axis.text.x=element_text(size=15), 
+        axis.text.y=element_text(size=15)) + 
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), 
+        panel.background=element_blank(), axis.line=element_line(colour="black"))+
+  scale_fill_brewer(palette = 'Dark2')
+  scale_x_discrete(name="Treatment", labels=c("Control", "Fence", "Plate"))
+
+## so its clear that the plate and fence treament have less volume lots
+
+# our hypothesis would be that this is due to some guilds of dunf beetle beign blocked from acting on the dung
+
+## but can we show the different betrween the levels statistically?
+
+##testing differences between the three treatments
+# Tukey test
+treatmentMCP <- mcp(treatment = 'Tukey')
+tuk <- glht(model1, treatmentMCP)
+confint(tuk)
+tuk
+par(mar=c(5.1, 6.2, 4.1, 2.1))
+plot(tuk)
+summary(tuk)
+## from this we can clearly see that the difference is between the controls nd either the plate or fence treatment
+## not between those two treatments
+
+### previously I did lsmeans to find the mean when account for rnadom effects
+## as of 2024 this package no lobnger lseems to eb in effect - how to sort?
 ### lsmeans from model 3
 lsmeans(model3, "treatment")
 
-wald.test(b=fixef(model3), Sigma=vcov(model3), Terms = 2, df=73)
 
-#### PART 2: comparing dung removal in control and the combined treatments
+
+#### PART 2: comparing dung removal in control and the combined treatments ####
 ## combining levels
 #library(plyr)
 #df2 <- data.frame(dung$Round, dung$plot, dung$treatment, dung$vlost)
